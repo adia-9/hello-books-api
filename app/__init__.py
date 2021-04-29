19e3c754bc22d0a204cc131aeb43b70b596b22b1
Aida <aida.rysbek@gmail.com>
2021-04-29 16:28:30 -0700

    two initial endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    app = Flask(__name__)
+    app = Flask(__name__) #creating a new flask callled app
 
+    from .routes import hello_world_bp #from file routes import variable hello-w...
+    app.register_blueprint(hello_world_bp) 
+    #register that var flask, so it knows to use this bluepr for our routes
+    
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,19 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)
+
+#define endpoint
+#with BP defining route /hello-world and it's gonna accept only GET request. ->#GET/hello-world
+#Defining what http method will be used:GET
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def get_hello_world():
+    my_response = "Hello, world!"
+    return my_response
+
+@hello_world_bp.route("/hello-world/JSON", methods = ["GET"])
+def hello_world_json():
+    return {
+        "name":"Aida",
+        "message":"My first message",
+        "hobbies":["Coding, writing"],
+    }, 201
